43431e7018759c621e5e537d4e7d3bab18b10e65
Akira Taguchi <akirahattivatti@gmail.com>
2019-12-29 16:18:05 +0200

    tilin iän kertominen toimii

--- bot.js
+++ bot.js
@@ -8,20 +8,40 @@ client.on('message', msg => {
         split = msg.content.split(' ');
         switch (split[1]) {
             case 'oglevel':
+                if (split[2] == undefined) {
+                    msg.reply('Hmm...taisit kutsua minua väärin.' +
+                        'Koita `Franku apuva` (tai käy uudestaan peruskoulu)');
+                    return;
+                }
                 const options = {
-                    url: 'https://www.habbo.fi/api/public/users?name=mag123love',
-                    method: 'GET'
+                    url: 'https://www.habbo.fi/api/public/users?name=' + split[2],
+                    method: 'GET',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Accept-Charset': 'utf-8',
+                        'User-Agent': 'HABBO client'
+                    }
                 };
-
+                var error = false;
                 request(options, function (err, res, body) {
                     let json = JSON.parse(body);
-                    console.log(json);
+                    if (typeof json.memberSince != 'undefined') {
+                        msg.reply('Henkilön ' + split[2] + ' tili on tehty tällöin: ' + json.memberSince);
+                    } else {
+                        msg.reply('Hmm...taisit kutsua minua väärin.' +
+                            'Koita `Franku apuva` (tai käy uudestaan peruskoulu)');
+                    }
                 });
+                return
+            case 'apuva':
+                msg.reply('haha nobo eiku\nKomentoni ovat:\n' +
+                    '`Franku apuva` - >rekursio syvenee hehe\n' +
+                    '`Franku oglevel <henkilön_id>` - Kertoo käyttäjän OG levelin/tilin iän');
                 return;
-            default: msg.reply('Hmm...taisit kutsua minua väärin. Koita ```Franku apuva``` (tai käy uudestaan peruskoulu)');
+            default: msg.reply('Hmm...taisit kutsua minua väärin.' +
+                'Koita `Franku apuva` (tai käy uudestaan peruskoulu)');
         }
     }
-
 });
 
 client.login(auth.token);
